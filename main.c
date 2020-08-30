35fb9fb329306fc5e4f75f9f91212fecf231d7ce
Xerbo <xerbo@protonmail.com>
2020-08-30 15:10:24 +0100

    Add some code

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// A function that should return, but never does
+int warning_function(){
+	for(int i = 0; i < 10; i++){
+		printf("Hello %d\n", i);
+	}
+}
+
+int main(){
+	printf("Hello World!\n");
+
+	warning_function();
+
+	return 0;
+}
